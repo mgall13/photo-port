8db1e2120b2f380e2517ff2cc5957910c9c17e8b
Kyro <mariogallardo1331@yahoo.com>
2022-03-27 17:36:00 -0500

    creating handleSubmit function

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    console.log(formState)
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 {/* // name input  */}
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name"  />
                 </div>
                 {/* // email input  */}
                 <div>
                     <label htmlFor="email">Email Address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} onChange={handleChange} name="email"  />
                 </div>
                 {/* // message text input  */}
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
                 </div>
                 <button type="submit">Submit</button>
             </form>
